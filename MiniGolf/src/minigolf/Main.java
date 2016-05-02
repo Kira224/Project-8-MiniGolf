eef388652ba7b27384ebca50d25a34e776265e52
Janna Barry <aeris224@hotmail.com>
2016-05-02 16:33:16 -0400

    Added null returns as stubs for now

--- MiniGolf/src/minigolf/Main.java
+++ MiniGolf/src/minigolf/Main.java
@@ -15,7 +15,32 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    }
+  
+    //skeleton classes
+    // creates and stores a new round object and returns reference to Round object
+    public static Round newRound(String courseName, String playerName){
+        return null;
+           
+    }
+    // looks up Round object
+    public static Round getRound(String player, String coursename){
+        return null;
+    }
+    //returns course object
+    public static Course getCourse(String courseName){
+        return null;
+    
+    }
+    //looks up round information and returns score so far at swipe station
+    public static int scoreSoFar(String courseName, String playerName){
+        return 0;
+    
+    }
+    //redundant method, will also be used by swipe station to enter score
+    public void enterStrokes(String courseName, String playerName, int strokes){
+    
     }
     
 }
