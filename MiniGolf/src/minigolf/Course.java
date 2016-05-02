4a7244b6eb6588969df77d4e897703a6d81ef83e
Janna Barry <aeris224@hotmail.com>
2016-05-02 16:02:53 -0400

    Adding basic skeleton code

---
+++ MiniGolf/src/minigolf/Course.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minigolf;
+
+/**
+ *
+ * @author Kira
+ */
+public class Course {
+    // skeleton code
+    String getName();
+    int getNumberofHoles();
+    int parForHole(int hole);
+    
+}
